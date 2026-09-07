4816fffc2d72702b65b6ff5289772c52bc5e2339
inntanint <inntannint@gmail.com>
2026-09-07 17:19:37 +0700

    update diamond

--- diamond-data.js
+++ diamond-data.js
@@ -6,6 +6,6 @@ const ITEMS = [
     {name:"275 Diamond", img:"images/52.3.jpeg", price:90000},
     {name:"570 Diamond", img:"images/52.4.jpeg", price:180000},
     {name:"1500 Diamond", img:"images/52.5.jpeg", price:360000},
-    {name:"4000 Diamond", img:"images/52.6.jpeg", price:870000},
+    {name:"4000 Diamond", img:"images/52.6.jpeg", price:880000},
 
 ];
